953186785652eec40e74a643bf9cb1a8fed2a039
JungHyaechang <hch9097@gmail.com>
2024-12-24 20:46:42 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.c
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.c
@@ -4,6 +4,9 @@
 
 int solution(int num1, int num2) {
     int answer = 0;
-    answer = num1 - num2;
+    
+    if ((-50000<= num1 <= 50000)&&(-50000 <= num2 <= 50000))
+        answer = num1 - num2;
+    
     return answer;
 }
