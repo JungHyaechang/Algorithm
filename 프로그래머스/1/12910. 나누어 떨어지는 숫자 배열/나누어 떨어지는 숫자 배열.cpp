b9f1d6a1d964625ae9558d7551e08b203c5a05c0
JungHyaechang <hch9097@gmail.com>
2024-12-29 05:22:47 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.41 ms, Memory: 6.02 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void SelectionSort(vector<int>& arr) {
+	for (int i = 0; i < arr.size() - 1; i++) {
+		int minIdx = i;
+		for (int j = i + 1; j < arr.size(); j++) {
+			if (arr[minIdx] > arr[j]) {
+				minIdx = j;
+			}
+		}
+		if (minIdx != i) {
+			int temp = arr[minIdx];
+			arr[minIdx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+}
+
+vector<int> solution(vector<int> array, int divisor) {
+
+	vector<int> answer;
+
+	for (int i = 0; i < array.size(); i++) {
+		if (array[i] % divisor == 0) {
+			answer.push_back(array[i]);
+		} 
+	}
+    if (answer.size() == 0) {
+		answer.push_back(-1);
+	}
+    
+	SelectionSort(answer);
+
+	
+		
+	return answer;
+}
