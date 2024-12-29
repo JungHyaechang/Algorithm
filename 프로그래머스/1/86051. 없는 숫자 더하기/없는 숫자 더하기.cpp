10565f77917d75c0b9951657d50df571b465cf59
JungHyaechang <hch9097@gmail.com>
2024-12-29 19:42:01 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int sum = 0;
+    
+    for(int i = 0; i < numbers.size(); i++){
+        sum += numbers[i];
+    }
+    
+    int answer = 45-sum;
+    return answer;
+}
