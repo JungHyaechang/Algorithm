f345211b260e5cfe4cc6483b9ade88cbae274b05
JungHyaechang <hch9097@gmail.com>
2024-12-29 20:39:06 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer;
+    int num = s.size();
+    if(num % 2 == 0){
+        answer.push_back(s[num/2 - 1]);
+        answer.push_back(s[num/2]);
+    } 
+    else {
+        answer.push_back(s[num/2]);
+    }
+    
+    return answer;
+}
