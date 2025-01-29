de426d3eb35b6a208d17ce61554e542ab24ce676
JungHyaechang <hch9097@gmail.com>
2025-01-29 19:49:07 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 29일 19:45:23
+2025년 01월 29일 19:49:03
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -9,17 +9,9 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 	
 	unordered_map<int, int> umap;
 
-	for (int i = 1; i <= n; i++) {
-		umap[i] = 1;
-	}
-
-	for (int j = 0; j < lost.size(); j++) {
-		umap[lost[j]]--;
-	}
-	
-	for (int k = 0; k < reserve.size(); k++) {
-		umap[reserve[k]]++;
-	}
+	for (int i = 1; i <= n; i++) umap[i] = 1;
+	for (int j = 0; j < lost.size(); j++) umap[lost[j]]--;	
+	for (int k = 0; k < reserve.size(); k++) umap[reserve[k]]++;
 
 	for (int m = 1; m <= n; m++) {
 		if (umap[m] == 0) {
